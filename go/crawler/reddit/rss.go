d218ff7562bfc355fa6be2fd66aa505dbee25c9d
ryansiau <rsiautama@gmail.com>
2025-05-21 02:15:25 +0700

    add client to fetch from reddit

---
+++ go/crawler/reddit/rss.go
@@ -0,0 +1,74 @@
+package reddit
+
+import (
+	"encoding/xml"
+	"fmt"
+
+	"resty.dev/v3"
+)
+
+func FetchRSS(subreddit string) (*Feed, error) {
+	res := Feed{}
+
+	resp, err := resty.New().R().SetResult(&res).Get("https://www.reddit.com/r/" + subreddit + "/.rss")
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode() != 200 {
+		fmt.Printf("error: %v\n", resp.String())
+		return nil, fmt.Errorf("status code: %d", resp.StatusCode())
+	}
+
+	return &res, nil
+}
+
+type Feed struct {
+	XMLName  xml.Name `xml:"feed"`
+	Text     string   `xml:",chardata"`
+	Xmlns    string   `xml:"xmlns,attr"`
+	Media    string   `xml:"media,attr"`
+	Category struct {
+		Text  string `xml:",chardata"`
+		Term  string `xml:"term,attr"`
+		Label string `xml:"label,attr"`
+	} `xml:"category"`
+	Updated string `xml:"updated"`
+	Icon    string `xml:"icon"`
+	ID      string `xml:"id"`
+	Link    []struct {
+		Text string `xml:",chardata"`
+		Rel  string `xml:"rel,attr"`
+		Href string `xml:"href,attr"`
+		Type string `xml:"type,attr"`
+	} `xml:"link"`
+	Logo     string `xml:"logo"`
+	Subtitle string `xml:"subtitle"`
+	Title    string `xml:"title"`
+	Entry    []struct {
+		Text   string `xml:",chardata"`
+		Author struct {
+			Text string `xml:",chardata"`
+			Name string `xml:"name"`
+			URI  string `xml:"uri"`
+		} `xml:"author"`
+		Category struct {
+			Text  string `xml:",chardata"`
+			Term  string `xml:"term,attr"`
+			Label string `xml:"label,attr"`
+		} `xml:"category"`
+		Content xml.CharData `xml:"content"`
+		ID      string       `xml:"id"`
+		Link    struct {
+			Text string `xml:",chardata"`
+			Href string `xml:"href,attr"`
+		} `xml:"link"`
+		Updated   string `xml:"updated"`
+		Published string `xml:"published"`
+		Title     string `xml:"title"`
+		Thumbnail struct {
+			Text string `xml:",chardata"`
+			URL  string `xml:"url,attr"`
+		} `xml:"thumbnail"`
+	} `xml:"entry"`
+}
---
+++ go/crawler/reddit/subreddit.go
@@ -0,0 +1,56 @@
+package reddit
+
+import (
+	"fmt"
+
+	"resty.dev/v3"
+)
+
+func FetchSubreddit(subreddit string) (*Subreddit, error) {
+	res := Subreddit{}
+
+	resp, err := resty.New().R().SetResult(&res).Get("https://www.reddit.com/r/" + subreddit + ".json")
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode() != 200 {
+		fmt.Printf("error: %v\n", resp.String())
+		return nil, fmt.Errorf("status code: %d", resp.StatusCode())
+	}
+
+	return &res, nil
+}
+
+type Subreddit struct {
+	Kind string `json:"kind"`
+	Data struct {
+		Children []struct {
+			Kind string `json:"kind"`
+			Data Post   `json:"data"`
+		} `json:"children"`
+		After   string `json:"after"`
+		Before  string `json:"before"`
+		Dist    int    `json:"dist"`
+		Modhash string `json:"modhash"`
+	} `json:"data"`
+}
+
+type Post struct {
+	Title       string  `json:"title"`
+	Subreddit   string  `json:"subreddit"`
+	Author      string  `json:"author"`
+	URL         string  `json:"url"`
+	Permalink   string  `json:"permalink"`
+	Score       int     `json:"score"`
+	NumComments int     `json:"num_comments"`
+	CreatedUTC  float64 `json:"created_utc"`
+	SelfText    string  `json:"selftext"`
+	Thumbnail   string  `json:"thumbnail"`
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	UpvoteRatio float64 `json:"upvote_ratio"`
+	Stickied    bool    `json:"stickied"`
+	Over18      bool    `json:"over_18"`
+	IsSelf      bool    `json:"is_self"`
+}
