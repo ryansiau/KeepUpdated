4aed8db7be820f0ce35c46e43317a6e7b52c7b39
ryansiau <rsiautama@gmail.com>
2025-05-21 01:18:55 +0700

    add looper

---
+++ go/scripting/looper.go
@@ -0,0 +1,73 @@
+package scripting
+
+import (
+	"time"
+)
+
+type Looper[T any] interface {
+	SetThreadCount(count int)
+	SetDelay(delay time.Duration)
+	SetGracefulShutdown(gs GracefulShutdown)
+	Loop(data []T, f func(data T))
+	WaitFinish()
+}
+
+type looper[T any] struct {
+	threadCount      int
+	sm               chan struct{}
+	delay            time.Duration
+	gracefulShutdown GracefulShutdown
+}
+
+func NewLooper[T any]() Looper[T] {
+	return &looper[T]{
+		threadCount: 1,
+	}
+}
+
+func (l *looper[T]) SetThreadCount(count int) {
+	l.threadCount = count
+}
+
+func (l *looper[T]) SetDelay(d time.Duration) {
+	l.delay = d
+}
+
+func (l *looper[T]) SetGracefulShutdown(gs GracefulShutdown) {
+	l.gracefulShutdown = gs
+}
+
+func (l *looper[T]) WaitFinish() {
+	if l.sm == nil {
+		return
+	}
+
+	for i := 0; i < l.threadCount; i++ {
+		<-l.sm
+	}
+}
+
+func (l *looper[T]) Loop(data []T, f func(data T)) {
+	l.sm = make(chan struct{}, l.threadCount)
+
+	for i := 0; i < l.threadCount; i++ {
+		l.sm <- struct{}{}
+	}
+
+	for _, d := range data {
+		<-l.sm
+
+		go func() {
+			defer func() {
+				l.sm <- struct{}{}
+			}()
+			f(d)
+		}()
+
+		if l.gracefulShutdown != nil && l.gracefulShutdown.IsTerminated() {
+			break
+		}
+	}
+
+	l.WaitFinish()
+}
